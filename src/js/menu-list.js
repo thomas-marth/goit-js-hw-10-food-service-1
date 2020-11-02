94bba1cab4f2287c142a78bdd8ca676f2a14ddb6
Nata Pryhunova <prygunova@gmail.com>
2020-11-02 23:13:56 +0200

    Update menu-list.js

--- src/js/menu-list.js
+++ src/js/menu-list.js
@@ -7,6 +7,29 @@ const Theme = {
 };
 
 const menuContainerRef = document.querySelector('.js-menu');
+const toggleSwitch = document.querySelector('#theme-switch-toggle');
+const bodyStyle = document.body.classList;
 
 const createMenuMarkup = menuListTpl(menuList);
 menuContainerRef.insertAdjacentHTML('beforeend', createMenuMarkup);
+
+
+onActiveTheme();
+toggleSwitch.addEventListener('change', onToggleTheme);
+
+function onToggleTheme(e) {
+  e.currentTarget.checked
+    ? bodyStyle.add(Theme.DARK) & bodyStyle.remove(Theme.LIGHT)
+    : bodyStyle.add(Theme.LIGHT) & bodyStyle.remove(Theme.DARK);
+  localStorage.setItem('theme', bodyStyle.value);
+}
+
+function onActiveTheme() {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme) {
+    bodyStyle.add(savedTheme);
+  }
+  if (bodyStyle.value === Theme.DARK) {
+    toggleSwitch.checked = true;
+  }
+}
